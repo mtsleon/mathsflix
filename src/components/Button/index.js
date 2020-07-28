72e0a6bbb522dfb3d5216bde7368ae290b986835
Matheus Leonardo <mtsleonardo.prog@gmail.com>
2020-07-28 01:47:45 -0300

    Setup inicial do projeto

---
+++ src/components/Button/index.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const Button = styled.button `
+    color: var(--white);
+    border: 1px solid var(--white);
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 16px 24px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    outline: none;
+    border-radius: 5px;
+    text-decoration: none;
+    display: inline-block;
+    transition: opacity .3s;
+
+    @media (max-width: 800px) {
+        & {
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: var(--primary);
+            border-radius: 0;
+            border: 0;
+            text-align: center;
+        }
+    }
+
+    &:hover,
+    &:focus {
+        opacity: .5;
+    }
+
+
+`;
+
+export default Button;
